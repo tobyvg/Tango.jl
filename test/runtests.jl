b2f397e66ad2850825d57e92019760a05c30b19c
tobyvg <tobyvangastelen@gmail.com>
2024-04-16 13:52:53 +0200

    Added testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Tango
 using Test
 
-@testset "Tango.jl" begin
-    # Write your tests here.
+@testset "Numerical derivative" begin
+    f(x) = 2*x + 1
+    g(x) = x^3 - x
+
+    @test numderiv(f,3,1) == 2
+    @test numderiv(g,2) ≈ 11 atol  = 0.01
 end
